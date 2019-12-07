7f72a9b17f9d1bb61f9506a1b10b2d5f6b93f972
peleg11 <shay.peleg11@gmail.com>
2019-12-07 21:48:24 +0200

    hompage render try3

--- SDate/settings.py
+++ SDate/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'SDate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/Users/user/Dev/project5/src/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
