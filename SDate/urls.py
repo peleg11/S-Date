af9b76096848dc593f96f1ce11809bb644d9ba35
peleg11 <shay.peleg11@gmail.com>
2019-12-07 21:24:06 +0200

    home page render try

--- SDate/urls.py
+++ SDate/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from
 from django.contrib import admin
 from django.urls import path
 
+from src.pages.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view)
 ]
