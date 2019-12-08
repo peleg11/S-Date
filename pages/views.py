a943111a9b3dc7710654f541717dd6b3ba4c0687
peleg11 <shay.peleg11@gmail.com>
2019-12-08 14:20:20 +0200

    delete

--- pages/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-# Create your views here.
-def home_view(request, *args, **kwargs):
-    # return HttpResponse("<h1> SDate HOME <h1>")
-    return render(request, "home.html", {})
