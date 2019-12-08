124310d9606710345fb0c83128bbb1d7a9bb3e70
peleg11 <shay.peleg11@gmail.com>
2019-12-08 16:02:36 +0200

    first app

---
+++ s_date_project/first_app/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from first_app import views
+
+app_name = 'first_app'
+
+urlpatterns = [
+    url(r'^register/$', views.register, name='reg_page'),
+    url(r'^$',views.index,name='home_page'),
+    url(r'^user_login/$', views.user_login, name='user_login'),
+    url(r'^$',views.base, name='base'),
+]
